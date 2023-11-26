b285886e3e98387b7ae48ed9082af3a747aeca1e
Javad <Javad.esmatib@Gmail.com>
2023-11-26 20:32:51 +0100

    Config tailwind

--- src/app/Components/NavLinks.jsx
+++ src/app/Components/NavLinks.jsx
@@ -5,7 +5,9 @@ import Link from 'next/link'
 const NavLinks = ({ href, title }) => {
     return (
         <Link
-            href={href} className="block py-2 pl-3 pr-4 text-accent-1 sm:text-xl rounded md:p-0 hover:text-content">
+            href={href} 
+            className="block py-2 pl-3 pr-4 text-accent-1 sm:text-xl rounded md:p-0 hover:text-content"
+            >
             {title}
         </Link>
 

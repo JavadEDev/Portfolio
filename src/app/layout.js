991c39d2dfb7e6e1adf3d2e7117dfdf831e41211
Javad <Javad.esmatib@Gmail.com>
2023-12-20 23:02:21 +0100

    fix top btn for mobile view

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,50 +1,47 @@
 import './globals.css'
-import Head from 'next/head';
 import { Roboto } from 'next/font/google'
 import Providers from './providers'
-import { NextSeo } from 'next-seo';
 
 const roboto = Roboto({
   weight: '400',
   subsets: ['latin'],
   display: 'swap',
 })
-
 export const metadata = {
   title: `Javad Esmati Portfolio Exploration`,
-  description: 'Javad Esmati, Frontend, Developer, Nextjs, Reactjs, TailwindCSS, Animation, Framer motion, Threejs, JavaSCript, HTML5, CSS3',
+  description: 'This is Javad Esmati portfolio website',
+  generator: 'Javad Esmati',
+  applicationName: 'Portfolio',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Javad Esmati', 'Developer', 'Frontend','Next.js', 'React', 'JavaScript'],
+  authors: [{ name: 'Javad' }, { name: 'Javad Esmati', url: 'https://javadesmati.name' }],
+  creator: 'Javad Esmati',
+  publisher: 'Javad',
+  formatDetection: {
+    email: 'Javad.esmatib@Gmail.com',
+    address: 'Norway',
+    telephone: false,
+  },
+  verification: {
+    google: 'google',
+    yandex: 'yandex',
+    yahoo: 'yahoo',
+    other: {
+      me: ['javad.esmatib@Gmail.com', 'www.javadesmati.name'],
+    },
+  },
 }
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={roboto.className}>
-        <Head>
-          <NextSeo
-            title="Javad Esmati Portfolio Exploration"
-            description="This is Javad Esmati portfolio website"
-            canonical="https://Javadesmati.name"
-          />
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{
-              __html: `
-            {
-              "@context": "https://schema.org",
-              "@type": "WebPage",
-              "name": "Javad Esmati Portfolio",
-              "description": "Javad Esmati as a frontend developer",
-              "url": "https://Javadesmati.name"
-            }
-          `,
-            }}
-          />
-          <title>Javad&rsquo;s Portfolio</title>
-          <meta name="description" content="This is Javad Esmati portfolio website" />
-          <meta name="keywords" content="Javad, Esmati, Frontend, backend, Developer, Programer, JavaSCript, HTML5, CSS3, Nextjs, Reactjs, MangoDB" />
-          <link rel="canonical" href="https://Javadesmati.name" />
-          <meta name="robots" content="index, follow" />
-        </Head>
+        <title>Javad&rsquo;s Portfolio</title>
+        <meta name="description" content="This is Javad Esmati portfolio website" />
+        <meta name="keywords" content="Javad, Esmati, Frontend, backend, Developer, Programer, JavaSCript, HTML5, CSS3, Nextjs, Reactjs, MangoDB" />
+        <link rel="canonical" href="https://Javadesmati.name" />
+        <meta name="robots" content="index, follow" />
         <Providers>{children}</Providers>
       </body>
     </html>

75f5ae14e85720d090e6c8e7305f881222ded977
Javad <Javad.esmatib@Gmail.com>
2023-12-29 11:58:31 +0100

    fix SEO error

--- src/app/utils/siteMetadata.js
+++ src/app/utils/siteMetadata.js
@@ -1,22 +1,22 @@
 
-const siteMetadata = () => {
-    return {
-        title: 'Javad Esmati Portfolio Exploration',
-        author: 'Javad Esmati',
-        headerTitle: 'Portfolio website',
-        description: 'A portfolio website created with Next.js, Tailwind CSS and contentlayer',
-        language: 'en-us',
-        theme: 'system',
-        siteUrl: 'https://www.javadesmati.name',
-        siteLogo: '/logo.svg',
-        socialBanner: '/images/social-banner.png',
-        email: 'Javad.esmatib@Gmail.com',
-        github: 'https://github.com/Javad-EB',
-        facebook: 'https://www.facebook.com',
-        youtube: 'https://www.youtube.com',
-        linkedin: 'https://www.linkedin.com/in/javadesmatib/',
-        locale: 'en-US',
-    }
+const siteMetadata =
+{
+    title: 'Javad Esmati Portfolio Exploration',
+    author: 'Javad Esmati',
+    headerTitle: 'Portfolio website',
+    description: 'A portfolio website created with Next.js, Tailwind CSS and contentlayer',
+    language: 'en-us',
+    theme: 'system',
+    siteUrl: 'https://www.javadesmati.name',
+    siteLogo: '/logo.svg',
+    socialBanner: '/images/social-banner.png',
+    email: 'Javad.esmatib@Gmail.com',
+    github: 'https://github.com/Javad-EB',
+    facebook: 'https://www.facebook.com',
+    youtube: 'https://www.youtube.com',
+    linkedin: 'https://www.linkedin.com/in/javadesmatib/',
+    locale: 'en-US',
 }
+
 
 export default siteMetadata

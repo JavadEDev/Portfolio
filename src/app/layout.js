aa426747b389b96125239d03f136e8589da80768
Javad <Javad.esmatib@Gmail.com>
2023-12-29 11:01:40 +0100

    mov siteMetadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Roboto } from 'next/font/google'
 import Providers from './providers'
-import siteMetadata from './siteMetadata'
+import siteMetadata from './utils/siteMetadata'
 
 const roboto = Roboto({
   weight: '400',
